f357f19f9a5b980d10e27bdcf16d0bc8152273dc
lukasvdgaag <lukasvdgaag@gmail.com>
2024-02-24 16:06:50 +0100

    chose: Update .gitlab-ci.yml to java 17.

--- workshop_2/backend/bsn/build.gradle.kts
+++ workshop_2/backend/bsn/build.gradle.kts
@@ -12,7 +12,7 @@ group = "nl.hva"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -29,14 +29,16 @@ sonar {
     properties {
         property("sonar.projectKey", "se-specialization-2023-2_tse1_lukas-van-der-gaag_assignments_218091fd-f4d6-475a-b22e-fea1f85e42b1")
         property("sonar.projectName", "assignments")
+        property("sonar.plugins.downloadOnlyRequired", true)
         property("sonar.qualitygate.wait", true)
+        property("sonar.task.timeout", "600")
     }
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "21"
+        jvmTarget = "17"
     }
 }
 
