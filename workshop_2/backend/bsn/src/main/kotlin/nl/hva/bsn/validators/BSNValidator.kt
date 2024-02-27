fba86c197adb205841094081cc7f6c7964de7b8a
lukasvdgaag <lukasvdgaag@gmail.com>
2024-02-27 13:20:44 +0100

    refactor: Implement interface delegation into BSNValidator

--- workshop_2/backend/bsn/src/main/kotlin/nl/hva/bsn/validators/BSNValidator.kt
+++ workshop_2/backend/bsn/src/main/kotlin/nl/hva/bsn/validators/BSNValidator.kt
@@ -1,19 +1,14 @@
 package nl.hva.bsn.validators
 
-class BSNValidator : Validator {
+class BSNValidator : Validator by ValidationBuilder()
+        .with(LengthValidator(BSN_MIN_LENGTH, BSN_MAX_LENGTH))
+        .with(NumericValidator())
+        .with(ElevenTestValidator())
+        .build() {
 
     companion object {
         const val BSN_MIN_LENGTH = 8
         const val BSN_MAX_LENGTH = 9
     }
 
-    private val validator: Validator = ValidationBuilder()
-        .with(LengthValidator(BSN_MIN_LENGTH, BSN_MAX_LENGTH))
-        .with(NumericValidator())
-        .with(ElevenTestValidator())
-        .build()
-
-    override fun validate(bsn: String): Boolean {
-        return validator.validate(bsn)
-    }
 }
