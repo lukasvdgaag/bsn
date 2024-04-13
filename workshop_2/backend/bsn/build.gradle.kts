edeca9fd34949b619222c5b39e566c3bae2da59a
lukasvdgaag <lukasvdgaag@gmail.com>
2024-04-13 18:04:44 +0200

    initial commit

--- workshop_2/backend/bsn/build.gradle.kts
+++ workshop_2/backend/bsn/build.gradle.kts
@@ -25,8 +25,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // h2 database on runtime
+    runtimeOnly("com.h2database:h2:2.1.214")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
